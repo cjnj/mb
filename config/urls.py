608902f8716e24932c69670b6968b5f44a74036c
COREY FSDI-111 <Corey.Jonhson@sdgku.edu>
2024-03-19 23:31:31 -0400

    final report 112

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("posts/", include("posts.urls"))
+    path("posts/", include("posts.urls")),
+    path("", include("pages.urls"))
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
-    template_name = "templates/pages/home.html"
+    template_name = "pages/home.html"
         
 class AboutPageView(TemplateView):
-    template_name = "templates/pages/about.html"
+    template_name = "pages/about.html"
